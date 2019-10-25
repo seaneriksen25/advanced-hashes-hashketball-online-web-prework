7281877b5d91d05868da69152781a26a01189aca
Sean Eriksen <eriksensean@yahoo.com>
2019-10-25 15:43:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
 game_hash = {
   :home [:team_name => ["Brooklyn Nets"],
   :colors => ["Black", "White"], 
-  :players],
+  :players => []],
   
   :away [:team_name, :colors, :players]
 }
