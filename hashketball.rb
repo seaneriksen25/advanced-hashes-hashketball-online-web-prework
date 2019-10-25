6dccf3dfc4192c7ca000545ee0cc8dd2f6ef54fa
Sean Eriksen <eriksensean@yahoo.com>
2019-10-25 15:44:27 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,8 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+game_hash = {
+  :home [:team_name => ["Brooklyn Nets"],
+  :colors => ["Black", "White"], 
+  :players => []],
+  
+  :away [:team_name, :colors, :players]
+}
